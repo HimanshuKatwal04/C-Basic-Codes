2f3bc42ec0e7126171b82e2308f647d49dab7ca9
Himanshu Katwal <himanshukatwalnew@gmail.com>
2023-12-19 15:17:59 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	int a[50],n,max,min;
+	printf("Enter number of terms in an array");
+	scanf("%d", &n);
+	for(int x=1;x<=n;x++)
+	{
+		printf("Enter an element");
+		scanf("%d", &a[x]);
+	
+	
+	max=a[x];
+	min=a[x];
+	}
+	for(int y=1;y<=n;y++)
+	{
+		
+		if(a[y]>max)
+		{
+			max=a[y];
+		}
+		else if(a[y]<min)
+		{
+			min=a[y];
+		}
+	}
+	
+	printf("Maximum= %d",max);
+	printf("\n Minmimum=%d",min);
+}
