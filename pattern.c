fb8a822d566a25958d60959964af937d9822a7f6
Himanshu Katwal <himanshukatwalnew@gmail.com>
2023-12-19 14:42:14 +0530

    Updated pattern.c

--- pattern.c
+++ pattern.c
@@ -1,3 +1,5 @@
+//WAP TO PRINT A PYRAMID PATTERN
+
 #include<stdio.h>
 void main()
 {
@@ -6,14 +8,14 @@ void main()
 	printf("Enter the number of rows");
 	scanf("%d", &rows);
 	
-	for(int x=0;x<rows+1;x++)
+	for(int x=0;x<rows+1;x++)//LOOP FOR THE LOOP
 	{
-		for(int y=rows;y!=x;y--)
+		for(int y=rows;y!=x;y--)//LOOP FOR THE SPACES
 		{
 			printf("_");
 			
 		}
-		for(int z=0;z<(x*2)-1;z++)
+		for(int z=0;z<(x*2)-1;z++)//LOOP FOR THE CHARACTER
 		{
 			
 			printf("*");
