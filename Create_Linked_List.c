0549491c8ce7632c4f1aefc42897ae2970d50f9f
Himanshu Katwal <himanshukatwalnew@gmail.com>
2024-05-13 11:26:00 +0530

    Create Create_Linked_List.c

---
+++ Create_Linked_List.c
@@ -0,0 +1,78 @@
+//WAP to create three nodes of a linked list and traverse and print the data of each node
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void traverse_and_print_(struct Node *ptr)
+{
+    printf("The data and link are as folowed\n");
+    while(ptr!=0){
+        printf("%d , %d\n",ptr->data,ptr->next);
+        ptr=ptr->next;
+    }
+}
+int main()#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void traverse_and_print_(struct Node *ptr)
+{
+    printf("The data and link are as folowed\n");
+    while(ptr!=0){
+        printf("%d , %d\n",ptr->data,ptr->next);
+        ptr=ptr->next;
+    }
+}
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+   
+    head=(struct Node *)malloc(sizeof(struct Node));
+    second=(struct Node *)malloc(sizeof(struct Node));
+    third=(struct Node *)malloc(sizeof(struct Node));
+   
+    head->data=5;
+    head->next=second;
+   
+    second->data=10;
+    second->next=third;
+   
+    third->data=15;
+    third->next=NULL;
+   
+     traverse_and_print_(head);
+     return 0;
+}
+
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+   
+    head=(struct Node *)malloc(sizeof(struct Node));
+    second=(struct Node *)malloc(sizeof(struct Node));
+    third=(struct Node *)malloc(sizeof(struct Node));
+   
+    head->data=5;
+    head->next=second;
+   
+    second->data=10;
+    second->next=third;
+   
+    third->data=15;
+    third->next=NULL;
+   
+     traverse_and_print_(head);
+     return 0;
+}
