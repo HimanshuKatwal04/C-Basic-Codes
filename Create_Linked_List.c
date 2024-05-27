714c7e97fad1e608ba1a5b83b1055a97a44e6a9d
Himanshu Katwal <himanshukatwalnew@gmail.com>
2024-05-27 12:33:57 +0530

    Create_Linked_List.c

--- Create_Linked_List.c
+++ Create_Linked_List.c
@@ -1,41 +1,51 @@
-//WAP to create three nodes of a linked list and traverse and print the data of each node
+//WAP to create a user defined linked list and traverse and print the data of each node
 
 #include<stdio.h>
 #include<stdlib.h>
-
-struct Node{
-    int data;
-    struct Node *next;
-};
-
-void traverse_and_print_(struct Node *ptr)
-{
-    printf("The data and link are as folowed\n");
-    while(ptr!=0){
-        printf("%d , %d\n",ptr->data,ptr->next);
-        ptr=ptr->next;
-    }
-}
-
-int main()
-{
-    struct Node *head;
-    struct Node *second;
-    struct Node *third;
-   
-    head=(struct Node *)malloc(sizeof(struct Node));
-    second=(struct Node *)malloc(sizeof(struct Node));
-    third=(struct Node *)malloc(sizeof(struct Node));
-   
-    head->data=5;
-    head->next=second;
-   
-    second->data=10;
-    second->next=third;
-   
-    third->data=15;
-    third->next=NULL;
-    
-    traverse_and_print_(head);
-    return 0;
-}
+ struct node{
+ 	int data;
+ 	struct node *next;
+ 	}*head,*temp,*newnode;
+ 	
+ void main()
+ {
+ 	void create();
+ 	create();
+    void traverse();
+    traverse();
+ }
+ 
+ void create()
+ {
+     char ch;
+ 	do
+ 	{	
+ 		
+ 		newnode=(struct node *)malloc(sizeof(struct node));
+ 		printf("Enter the data in the node\n");
+ 		scanf("%d",&newnode->data);
+ 		newnode->next=NULL;
+ 		
+ 		if(head==NULL)
+ 		{
+ 			head=newnode;
+ 			temp=newnode;
+ 		}
+ 		else
+ 		{
+ 			temp->next=newnode;
+ 			temp=newnode;
+ 		}
+ 		printf("Enter y/n for newnode\n");
+ 		scanf("%s",&ch);
+ 	}while(ch=='y'||ch=='Y');
+ }
+ void traverse()
+           {
+              temp=head;
+              while(temp!=0)
+              {
+                printf("%d\n",temp->data);
+                temp=temp->next;
+              }
+          }
